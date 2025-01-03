a0c0c69f2d9c33c47c2e3efdae4358dd3ceb20be
Dalton-K-Hughes <dhughes3223@gmail.com>
2025-01-03 18:26:30 -0500

    Testing ways to wait for elements to appear without time.sleep, currently stuck with the first test box enters the info then just pauses

--- job_scraper.py
+++ job_scraper.py
@@ -1,34 +1,68 @@
 import time
 
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()  # Optional argument, if not specified will search path.
-
-browser.get('https://www.indeed.com')
+options = webdriver.ChromeOptions()  # Optional argument, if not specified will search path.
+options.add_experimental_option('detach', True)
+browser = webdriver.Chrome(options=options)
+browser.get('https://www.indeed.com/')
 time.sleep(2) # Let the user actually see something!
 
-job_searchbox = browser.find_element(By.NAME, 'q')
-job_searchbox.send_keys('it')
+try:
+    job_searchbox = WebDriverWait(browser, 500).until(EC.presence_of_element_located(
+        By.XPATH, "//input[@name='q']")).send_keys('it')
+except NoSuchElementException:
+    browser.quit()
+finally:
+    job_searchbox = browser.find_element(By.XPATH, "//input[@name='q']").send_keys('it')
 
-location_searchbox = browser.find_element(By.NAME, 'l')
-location_searchbox.send_keys(Keys.CONTROL + 'a')
-time.sleep(1)
-location_searchbox.send_keys(Keys.DELETE)
-time.sleep(1)
-location_searchbox.send_keys('Savannah, GA')
-time.sleep(1)
-submit_btn = browser.find_element(By.CLASS_NAME, 'yosegi-InlineWhatWhere-primaryButton').click()
+# job_searchbox = browser.find_element(By.NAME, 'q')
+# job_searchbox.send_keys('it')
 
-time.sleep(10)
+try:
+    location_searchbox = WebDriverWait(browser, 500).until(EC.presence_of_element_located(
+        By.XPATH, "//input[@name='l']"))
+    location_searchbox.send_keys(Keys.CONTROL + 'a')
+    time.sleep(1)
+    location_searchbox.send_keys(Keys.DELETE)
+    time.sleep(1)
+    location_searchbox.send_keys('Savannah, GA')
+except NoSuchElementException:
+    browser.quit()
+finally:
+    location_searchbox = browser.find_element(By.NAME, 'l')
+    location_searchbox.send_keys(Keys.CONTROL + 'a')
+    time.sleep(1)
+    location_searchbox.send_keys(Keys.DELETE)
+    time.sleep(1)
+    location_searchbox.send_keys('Savannah, GA')
 
-# search_box = browser.find_element(By.NAME, 'q')
+# location_searchbox = browser.find_element(By.NAME, 'l')
+# location_searchbox.send_keys(Keys.CONTROL + 'a')
+# time.sleep(1)
+# location_searchbox.send_keys(Keys.DELETE)
+# time.sleep(1)
+# location_searchbox.send_keys('Savannah, GA')
+# time.sleep(1)
+try:
+    submit_btn = WebDriverWait(browser, 500).until(EC.presence_of_element_located(
+        By.XPATH, "//button[@type='submit']"
+    ))
+    submit_btn.click()
+except NoSuchElementException:
+    browser.quit()
+finally:
+    submit_btn = browser.find_element(By.CLASS_NAME, 'yosegi-InlineWhatWhere-primaryButton').click()
 
-# search_box.send_keys('The Devil Wears Prada')
+time.sleep(5)
 
-# search_box.submit()
-
-# time.sleep(5) # Let the user actually see something!
+# job_cards = browser.find_elements(By.CLASS_NAME, 'css-1ac2h1w eu4oa1w0')
+# for job_card in job_cards:
+#     print(job_card)
 
 # browser.quit()
